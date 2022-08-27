086e1b30c61ec4adcc1b32c9001707a1a7bca59b
Gabriel Szałajko <maflex24@gmail.com>
2022-08-28 00:33:51 +0200

    Added unit tests for all current map class methods

--- MapTests.cs
+++ MapTests.cs
@@ -13,7 +13,7 @@ namespace Warship.UnitTests
             var exampleRowsQty = 5;
             var map = new Map(columns, exampleRowsQty);
 
-            var mapColumns = map.ColumnsContext.Count;
+            var mapColumns = map.MapContext.Count;
 
             Assert.Equal(columns, mapColumns);
         }
@@ -26,12 +26,97 @@ namespace Warship.UnitTests
             var exampleColumnsQty = 10;
             var map = new Map(exampleColumnsQty, rows);
 
-            foreach (var column in map.ColumnsContext)
+            foreach (var column in map.MapContext)
             {
                 var rowContextListCount = column.Value.Length;
 
                 Assert.Equal(rows, rowContextListCount);
             }
+        }
+
+        [Theory]
+        [InlineData('A', 1)]
+        [InlineData('A', 10)]
+        [InlineData('J', 1)]
+        [InlineData('J', 10)]
+        public void IsCoordinateExist_ForValidValues_ReturnTrue(char x, int y)
+        {
+            var coordinate = new Coordinate(x, y);
+            var map = new Map(10, 10);
+
+            var coordinateExist = map.IsCoordinateExist(coordinate);
+
+            Assert.True(coordinateExist);
+        }
+
+        [Theory]
+        [InlineData('A', 11)]
+        [InlineData('K', 1)]
+        [InlineData('K', 11)]
+        public void IsCoordinateExist_ForWrongValues_ReturnFalse(char x, int y)
+        {
+            var coordinate = new Coordinate(x, y);
+            var map = new Map(10, 10);
+
+            var coordinateExist = map.IsCoordinateExist(coordinate);
+
+            Assert.False(coordinateExist);
+        }
+
+        [Fact]
+        public void WasCoordinateShooted_ForShooted_ReturnTrue()
+        {
+            var coordinate = new Coordinate('D', 10);
+            var map = new Map(10, 10);
+
+            map.MarkShoot(coordinate, true);
+
+            var mapFieldContext = map.MapContext[coordinate.X][coordinate.Y - 1];
+            var wasShooted =
+                mapFieldContext == map.hitChar ||
+                mapFieldContext == map.missedChar;
+
+            Assert.True(wasShooted);
+        }
+
+        [Fact]
+        public void WasCoordinateShooted_ForMissed_ReturnFalse()
+        {
+            var coordinate = new Coordinate('D', 10);
+            var map = new Map(10, 10);
+
+            var mapFieldContext = map.MapContext[coordinate.X][coordinate.Y - 1];
+            var wasShooted =
+                mapFieldContext == map.hitChar ||
+                mapFieldContext == map.missedChar;
+
+            Assert.False(wasShooted);
+        }
+
+        [Fact]
+        public void MarkShoot_ForSuccessfulShot_ReturnTrue()
+        {
+            var coordinate = new Coordinate('D', 10);
+            var map = new Map(10, 10);
+
+            map.MarkShoot(coordinate, true);
+
+            var isMarked = map.MapContext[coordinate.X][coordinate.Y - 1] == map.hitChar;
+
+            Assert.True(isMarked);
+        }
+
+        [Fact]
+        public void MarkShoot_ForMissedShot_ReturnTrue()
+        {
+            var coordinate = new Coordinate('D', 10);
+            var map = new Map(10, 10);
+
+            map.MarkShoot(coordinate, false);
+
+            var isMarked = map.MapContext[coordinate.X][coordinate.Y - 1] == map.missedChar;
+
+            Assert.True(isMarked);
         }
     }
 }
