22b14a57351f77047fead5b32e13e6682ed2ca8a
Gabriel Szałajko <maflex24@gmail.com>
2022-08-29 19:53:18 +0200

    Added coordinates test

---
+++ CoordinateTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Warship.Classes;
+using Xunit;
+
+namespace Warship.UnitTests
+{
+    public class CoordinateTests
+    {
+        [Theory]
+        [InlineData("B14")]
+        [InlineData("A1")]
+        [InlineData("Z19")]
+        public void CreateCoordinateFromInput_ForValidInputs_ReturnsCoordinate(string userInput)
+        {
+            var x = userInput[0];
+            var y = int.Parse(userInput.Substring(1));
+
+            var coordinate = new Coordinate(userInput);
+
+            Assert.Equal(x, coordinate.X);
+            Assert.Equal(y - 1, coordinate.Y);
+        }
+    }
+}
